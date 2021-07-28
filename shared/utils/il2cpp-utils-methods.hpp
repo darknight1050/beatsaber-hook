017c29ac27a4691530806a1795ff94cedbfb123d
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2021-07-28 19:59:42 +0200

    Update il2cpp-utils-methods.hpp

--- shared/utils/il2cpp-utils-methods.hpp
+++ shared/utils/il2cpp-utils-methods.hpp
@@ -373,9 +373,9 @@ namespace il2cpp_utils {
         }
     }
     template<class TOut = void, bool checkTypes = true, class T, class... TArgs>
-    requires (std::is_same_v<T, Il2CppClass*> || std::is_same_v<T, Il2CppType*> || std::is_same_v<T, std::nullptr_t>)
+    requires ((std::is_same_v<T, Il2CppClass*> || std::is_same_v<T, Il2CppType*> || std::is_same_v<T, std::nullptr_t>) && std::is_same_v<std::remove_reference_t<T>, T>)
     TOut RunMethodThrow(T instance, const MethodInfo* method, TArgs&& ...params) {
-        return RunMethodThrow<TOut, checkTypes, T&, TArgs...>(instance, method, std::forward<TArgs>(params)...);
+        return RunMethodThrow<TOut, checkTypes, T, TArgs...>(instance, method, std::forward<TArgs>(params)...);
     }
     #else
     /// @brief Instantiates a generic MethodInfo* from the provided Il2CppClasses.
