d5a332ce74a663e1357363e62df5ca5f3fa854c0
Adam ? <adamznow@gmail.com>
2022-05-22 16:57:23 -0700

    Disposable copy-ability for value types/copyable types, comment

--- shared/utils/typedefs-disposal.hpp
+++ shared/utils/typedefs-disposal.hpp
@@ -6,17 +6,20 @@
 namespace bs_hook {
     template<class T>
     concept has_dispose_vt = requires (T t) {
-        {t.Dispose();}
+        t.Dispose();
     };
     template<class T>
     concept has_dispose_rt = requires (T t) {
-        {t->Dispose();}
+        t->Dispose();
     };
-    template<class T>
+    /// @brief Holds an instance that can be disposed.
+    /// If holding a value type that can be copied, is copyable.
+    /// If holding a pointer, cannot be copied.
+    /// Note that the pointer should have a lifetime that is AT LEAST the lifetime of the Disposable instance.
+    template<class T, bool = !std::is_pointer_v<T> && std::is_copy_constructible_v<T>>
     requires (!std::is_reference_v<T> && (has_dispose_vt<T> || has_dispose_rt<T> || std::is_convertible_v<T, Il2CppObject*>))
     struct Disposable {
         Disposable(T t) : value(t) {}
-        Disposable(Disposable const&) = delete;
         explicit operator T() {return value;}
         operator T&() {return value;}
         T operator ->() {return value;}
@@ -33,5 +36,14 @@ namespace bs_hook {
         }
         private:
         T value;
+    };
+    template<class T>
+    requires (!std::is_reference_v<T> && (has_dispose_rt<T> || std::is_convertible_v<T, Il2CppObject*>))
+    struct Disposable<T, false> : Disposable<T, true> {
+        using Disposable<T, true>::Disposable;
+        Disposable(Disposable const&) = delete;
+        Disposable(Disposable&&) = default;
+        Disposable& operator=(Disposable&&) = default;
+        Disposable& operator=(Disposable const&) = default;
     };
 }
