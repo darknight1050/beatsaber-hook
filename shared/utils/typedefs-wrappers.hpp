f54b4b7f9073525d12d63a612942407a4726768c
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-07-12 14:13:36 -0400

    Fix ptr call

--- shared/utils/typedefs-wrappers.hpp
+++ shared/utils/typedefs-wrappers.hpp
@@ -507,7 +507,7 @@ struct SafePtrUnity : public SafePtr<T, true> {
             return other.isAlive() == isAlive();
         }
 
-        return static_cast<T*>(other.internalHandle) == static_cast<T*>(ptr());
+        return static_cast<T*>(other.internalHandle) == static_cast<T*>(Parent::ptr());
     }
 
     template<typename U = T>
@@ -516,19 +516,19 @@ struct SafePtrUnity : public SafePtr<T, true> {
             return static_cast<bool>(other) == isAlive();
         }
 
-        return static_cast<T*>(other) == static_cast<T*>(ptr());
+        return static_cast<T*>(other) == static_cast<T*>(Parent::ptr());
     }
 
 
 
     inline bool isAlive() {
 #ifdef HAS_CODEGEN
-        return ((bool)Parent::internalHandle) && (ptr()) && ptr().m_cachedPtr.m_value;
+        return ((bool)Parent::internalHandle) && (Parent::ptr()) && Parent::ptr().m_cachedPtr.m_value;
 #else
         // offset yay
         // the offset as specified in the codegen header of [m_cachedPtr] is 0x10
         // which is also the first field of the instance UnityEngine.Object
-      return ((bool)Parent::internalHandle) && (ptr()) && *reinterpret_cast<void**>(reinterpret_cast<uint8_t*>(ptr()) + 0x10);
+      return ((bool)Parent::internalHandle) && (Parent::ptr()) && *reinterpret_cast<void**>(reinterpret_cast<uint8_t*>(Parent::ptr()) + 0x10);
 #endif
     }
 
