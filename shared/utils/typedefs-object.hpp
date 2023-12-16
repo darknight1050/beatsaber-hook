d621fcb79819ccfefff10127172059a9daede690
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-12-16 21:01:35 +0100

    Undo include order shenanigans for Il2CppObject/System.Object

--- shared/utils/typedefs-object.hpp
+++ shared/utils/typedefs-object.hpp
@@ -2,32 +2,6 @@
 
 #include "type-concepts.hpp"
 
-#ifdef HAS_CODEGEN
-
-#ifdef USE_CODEGEN_FIELDS
-#define _HAD_CODEGEN_FIELDS
-#else
-#define USE_CODEGEN_FIELDS
-#endif
-
-#include "System/zzzz__Object_def.hpp"
-#ifndef _HAD_CODEGEN_FIELDS
-#undef USE_CODEGEN_FIELDS
-#endif
-
-#undef _HAD_CODEGEN_FIELDS
-
-typedef Il2CppClass Il2CppVTable;
-struct MonitorData;
-struct Il2CppObject : public System::Object {
-    union {
-        Il2CppClass *klass;
-        Il2CppVTable *vtable;
-    };
-    MonitorData *monitor;
-};
-#include "System/zzzz__Object_impl.hpp"
-#else
 typedef Il2CppClass Il2CppVTable;
 struct MonitorData;
 typedef struct Il2CppObject
@@ -39,5 +13,5 @@ typedef struct Il2CppObject
     };
     MonitorData *monitor;
 } Il2CppObject;
-#endif
+
 MARK_REF_PTR_T(Il2CppObject);
