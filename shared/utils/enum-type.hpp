91a5c634b67082ee8c0ba078de2b0505c1e500f1
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-12-23 02:07:33 +0100

    Add simple base types for enums and value types

---
+++ shared/utils/enum-type.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace bs_hook {
+    #pragma pack(push, 1)
+    struct EnumType {
+
+    };
+    #pragma pack(pop)
+}
---
+++ shared/utils/value-type.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#pragma pack(push, 1)
+namespace bs_hook {
+    struct ValueType {
+
+    };
+}
+#pragma pack(pop)
