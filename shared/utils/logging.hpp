32358370513fe700d7f970ac5c4c1334f9a3197d
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2024-03-13 21:09:39 -0400

    log dlerror safely

--- shared/utils/logging.hpp
+++ shared/utils/logging.hpp
@@ -5,3 +5,15 @@
 namespace il2cpp_utils {
 static constexpr auto Logger = ::Paper::ConstLoggerContext("beatsaber-hook_" VERSION);
 }
+
+//nullable string
+namespace bs_hooks {
+struct nullable {
+    const char* str;
+    explicit constexpr nullable(const char* s) : str(s) {}
+};
+
+constexpr auto format_as(nullable n) {
+    return n.str ? n.str : "(null)";
+}
+}  // namespace my
